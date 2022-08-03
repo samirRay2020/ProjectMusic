a69641b3e19837c3388627f69c793517df060101
Samir Ray <67779383+samirRay2020@users.noreply.github.com>
2022-08-03 15:23:32 +0530

    changed to take command line arguments

--- aws_test.sh
+++ aws_test.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-echo "Enter cluster name:"
-read clusterName
-echo "Enter service name:"
-read serviceName
 
-result="$(aws ecs describe-services --cluster $clusterName --service $serviceName --query 'services[].taskDefinition' | jq -r '.[0]')"
+result="$(aws ecs describe-services --cluster $1 --service $2 --query 'services[].taskDefinition' | jq -r '.[0]')"
 version=${result:0-1}
 previousVersion=$(expr $version - 1)
 result=${result/%$version/$previousVersion}
 
 if [[ $previousVersion -ne 0 ]]
 then 
-    aws ecs update-service --cluster $clusterName --service $serviceName --task-definition $result
+    aws ecs update-service --cluster $1 --service $2 --task-definition $result
     echo "Version changed from " $version " to "  $previousVersion " successfully."
 else
     echo "Version cannot be zero"
