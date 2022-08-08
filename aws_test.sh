8dfcda0f80d2cfe5783e193128476266c98cbeeb
Samir Ray <67779383+samirRay2020@users.noreply.github.com>
2022-08-08 13:13:31 +0530

    Update aws_test.sh

--- aws_test.sh
+++ aws_test.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-all_task_prod () {
-  result="$(aws ecs list-task-definitions --family-prefix $1 --query 'taskDefinitionArns')"
-  len=$(echo "$result" | jq '. | length')
-  for (( c=0; c<$len; c++ ))
-  do 
-    data=(echo "$result" | jq ".[$c]") 
-    aws ecs update-service --cluster $2 --service $3 --task-definition $data
-  done
-}
 
-specific_task () {
 result="$(aws ecs describe-services --cluster $1 --service $2 --query 'services[].taskDefinition' | jq -r '.[0]')"
 version=${result:0-1}
 previousVersion=$(expr $version - 1)
@@ -23,4 +13,4 @@ then
 else
     echo "Version cannot be zero"
 fi
-}
+
