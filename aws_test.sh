bee05024c399d774bef17cf416dd78a71285696f
Samir Ray <67779383+samirRay2020@users.noreply.github.com>
2022-08-08 12:54:18 +0530

    Update aws_test.sh

--- aws_test.sh
+++ aws_test.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+all_task () {
+result="$(aws ecs list-task-definitions --family-prefix Web-Definition-Second --query 'taskDefinitionArns')"
+  len=$(echo "$result" | jq '. | length')
+  for (( c=0; c<$len; c++ ))
+  do 
+    data=(echo "$result" | jq ".[$c]") 
+    aws ecs update-service --cluster AutoRollbackTestCluster --service all --task-definition $data
+  done
+}
+
+specific_task () {
 result="$(aws ecs describe-services --cluster $1 --service $2 --query 'services[].taskDefinition' | jq -r '.[0]')"
 version=${result:0-1}
 previousVersion=$(expr $version - 1)
@@ -12,3 +23,4 @@ then
 else
     echo "Version cannot be zero"
 fi
+}
