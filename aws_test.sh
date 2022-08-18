cdbafac3823577f84a5aa1f1b2ad31cda6b3419d
Samir Ray <67779383+samirRay2020@users.noreply.github.com>
2022-08-18 18:24:38 +0530

    Update aws_test.sh

--- aws_test.sh
+++ aws_test.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-if [[ $3 == "rollback" ]]
+if [[ $3 == "restart" ]]
 then
+    result="$(aws ecs describe-services --cluster $1 --service $2 --query 'services[].taskDefinition' | jq -r '.[0]')"
+    version=${result:0-1}
+#     previousVersion=$(expr $version - 1)
+#     result=${result/%$version/$previousVersion}
+
+    if [[ $version -ne 0 ]]
+    then 
+        aws ecs update-service --cluster $1 --service $2 --task-definition $result
+    else
+        echo "Version cannot be zero"
+    fi
+   
+else 
     result="$(aws ecs describe-services --cluster $1 --service $2 --query 'services[].taskDefinition' | jq -r '.[0]')"
     version=${result:0-1}
     previousVersion=$(expr $version - 1)
@@ -11,18 +24,6 @@ then
     then 
         aws ecs update-service --cluster $1 --service $2 --task-definition $result
         echo "Version changed from " $version " to "  $previousVersion " successfully."
-    else
-        echo "Version cannot be zero"
-    fi
-else 
-    result="$(aws ecs describe-services --cluster $1 --service $2 --query 'services[].taskDefinition' | jq -r '.[0]')"
-    version=${result:0-1}
-#     previousVersion=$(expr $version - 1)
-#     result=${result/%$version/$previousVersion}
-
-    if [[ $version -ne 0 ]]
-    then 
-        aws ecs update-service --cluster $1 --service $2 --task-definition $result
     else
         echo "Version cannot be zero"
     fi
