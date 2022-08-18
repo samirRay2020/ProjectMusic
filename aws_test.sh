0afb1856cfc437b4295028bf5c0918a096828446
Samir Ray <67779383+samirRay2020@users.noreply.github.com>
2022-08-18 18:28:47 +0530

    added restart version

--- aws_test.sh
+++ aws_test.sh
@@ -4,16 +4,12 @@ if [[ $3 == "restart" ]]
 then
     result="$(aws ecs describe-services --cluster $1 --service $2 --query 'services[].taskDefinition' | jq -r '.[0]')"
     version=${result:0-1}
-#     previousVersion=$(expr $version - 1)
-#     result=${result/%$version/$previousVersion}
-
     if [[ $version -ne 0 ]]
     then 
         aws ecs update-service --cluster $1 --service $2 --task-definition $result
     else
         echo "Version cannot be zero"
     fi
-   
 else 
     result="$(aws ecs describe-services --cluster $1 --service $2 --query 'services[].taskDefinition' | jq -r '.[0]')"
     version=${result:0-1}
@@ -28,5 +24,3 @@ else
         echo "Version cannot be zero"
     fi
 fi
-
-
